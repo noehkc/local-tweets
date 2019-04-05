56a47ac302f8b3da0133635058f3dd98381d9467
Noehkc <noetfd@gmail.com>
2019-04-05 14:38:33 -0500

    Added function to remove tweet from local storage

--- app.js
+++ app.js
@@ -34,15 +34,18 @@ function newTweet(e) {
 	li.appendChild(removeBtn);
 	tweetList.appendChild(li);
 	addTweetLocalStorage(tweet);
+
+	this.reset();
 }
 
 // Remove tweet
 function removeTweet(e){
 	if(e.target.classList.contains('remove-tweet')){
 					e.target.parentElement.remove();
-				}else{
-
 				}
+
+	//remove from storage
+	removeTweetLocalStorage( e.target.parentElement.textContent );			
 }
 
 //Add tweets to local storage
@@ -69,5 +72,40 @@ function getTweetsFromStorage(){
 
 function localStorageOnLoad(){
     let tweets = getTweetsFromStorage();
-    console.log(tweets);
+	
+	
+	//loop through local storage array and print value
+	tweets.forEach(function(tweet){
+		const removeBtn = document.createElement('a');
+		removeBtn.classList = 'remove-tweet';
+		removeBtn.textContent = "X";
+		
+		//create an <li> element
+		const li = document.createElement('li');
+		li.textContent = tweet;
+			//Add the remove button to each tweet
+		li.appendChild(removeBtn);
+		tweetList.appendChild(li);
+	})
+}
+
+//Remove tweet from local storage
+
+function removeTweetLocalStorage(tweet){
+	//get tweets from storage
+
+	let tweets = getTweetsFromStorage();
+
+	//Remove the x from the tweet
+	const tweetDelete = tweet.substring(0, tweet.length -1);
+
+	// Loop through the tweets and remove the tweet that is equal
+
+	tweets.forEach(function(tweetLS, index){
+		if(tweetDelete === tweetLS){
+			tweets.splice(index, 1);
+		}
+	});
+	//save the data
+	localStorage.setItem('tweets', JSON.stringify(tweets));
 }
